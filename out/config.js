a3bffe6ad53464d7474bfe0cd0452959a2f1d762
wanghan <wanghan.shenzhen@gmail.com>
2019-04-30 10:26:40 +0800

    clear git cache, update .gitignore

--- out/config.js
+++
@@ -1,3 +0,0 @@
-export const board = [50, 50];
-export const cellSize = [10, 10];
-export const initialSnakeSize = 5;
--- out/helpers.js
+++
@@ -1,4 +0,0 @@
-export const randomIndex = (length) => {
-    return Math.floor(Math.random() * length);
-};
-export const log = console.log.bind(console);
